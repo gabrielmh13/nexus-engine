ebfb592825ab051b1d3b13f78469f0010bdd9052
Gabriel Hernandes <gabrielmh13@hotmail.com>
2026-02-26 21:04:29 -0300

    refac ecs

--- engine/include/engine/ecs/Registry.hpp
+++ engine/include/engine/ecs/Registry.hpp
@@ -1,11 +1,17 @@
 #pragma once
-#include "engine/core/ecs/Entity.hpp"
+#include "engine/ecs/Entity.hpp"
+
+#include<vector>
 
 namespace NexusEngine::ECS {
     class Registry {
+    public:
         Entity CreateEntity();
         void DestroyEntity(Entity entity);
         bool IsEntityValid(Entity entity) const;
         uint32_t GetGeneration(uint32_t entityId) const;
-    }
+    private:
+        std::vector<uint32_t> m_Generations;
+        std::vector<uint32_t> m_FreeList;
+    };
 }
---
+++ engine/src/ecs/Registry.cpp
@@ -0,0 +1,19 @@
+#include "engine/ecs/Registry.hpp"
+
+namespace NexusEngine::ECS {
+    // Entity Registry::CreateEntity() {
+        
+    // }
+
+    // void Registry::DestroyEntity(Entity entity) {
+        
+    // }
+
+    // bool Registry::IsEntityValid(Entity entity) const {
+    //     return false;
+    // }
+
+    // uint32_t Registry::GetGeneration(uint32_t entityId) const {
+    //     return 0;
+    // }
+}
