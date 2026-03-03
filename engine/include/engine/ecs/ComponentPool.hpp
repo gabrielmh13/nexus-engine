4129aee86cd9382756fa5ac89be95a8a1a21e6b6
Gabriel Hernandes <gabrielmh13@hotmail.com>
2026-03-02 23:37:47 -0300

    feat: add component pool

---
+++ engine/include/engine/ecs/ComponentPool.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <unordered_map>
+#include <cstdint>
+
+namespace NexusEngine::ECS {
+    class IComponentPool {
+    public:
+        virtual ~IComponentPool() = default;
+        virtual void RemoveComponent(uint32_t entityId) = 0;
+    };
+
+    template<typename T>
+    class ComponentPool : public IComponentPool {
+    public:
+        T& Add(uint32_t entityId){
+            auto [it, _] = data.emplace(entityId, T{});
+            return it->second;
+        }
+
+        T& Add(uint32_t entityId, T component){
+            auto [it, _] = data.insert_or_assign(entityId, std::move(component));
+            return it->second;
+        }
+
+        T* Get(uint32_t entityId){
+            auto it = data.find(entityId);
+            return it != data.end() ? &it->second : nullptr;
+        }
+
+        void RemoveComponent(uint32_t entityId) override {
+            data.erase(entityId);
+        }
+    private:
+        std::unordered_map<uint32_t, T> data;
+    };
+}
