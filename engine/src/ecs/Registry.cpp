c38a78dbcf8ab084955ae0c881c8bb5c75e5c51c
Gabriel Hernandes <gabrielmh13@hotmail.com>
2026-03-02 21:03:24 -0300

    feat: add registry entity methods implementation

--- engine/src/ecs/Registry.cpp
+++ engine/src/ecs/Registry.cpp
@@ -1,19 +1,36 @@
 #include "engine/ecs/Registry.hpp"
+#include <cassert>
 
 namespace NexusEngine::ECS {
-    // Entity Registry::CreateEntity() {
-        
-    // }
+    Entity Registry::CreateEntity() {
+        uint32_t id;
 
-    // void Registry::DestroyEntity(Entity entity) {
-        
-    // }
+        if(!m_FreeList.empty()){
+            id = m_FreeList.back();
+            m_FreeList.pop_back();
+        } else {
+            id = m_Generations.size();
+            m_Generations.push_back(0);
+        }
 
-    // bool Registry::IsEntityValid(Entity entity) const {
-    //     return false;
-    // }
+        return {id, m_Generations[id]};
+    }
 
-    // uint32_t Registry::GetGeneration(uint32_t entityId) const {
-    //     return 0;
-    // }
+    void Registry::DestroyEntity(Entity entity) {
+        if(!IsEntityValid(entity))
+            return;
+
+        m_Generations[entity.m_Id] += 1;
+        m_FreeList.push_back(entity.m_Id);
+    }
+
+    bool Registry::IsEntityValid(Entity entity) const {
+        uint32_t id = entity.m_Id;
+        return id < m_Generations.size() && entity.m_Generation == m_Generations[id];
+    }
+
+    uint32_t Registry::GetGeneration(uint32_t entityId) const {
+        assert(entityId < m_Generations.size() && "Entity ID out of range");
+        return m_Generations[entityId];
+    }
 }
--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,6 +1,9 @@
 #include "engine/core/Application.hpp"
+#include "engine/ecs/Registry.hpp"
 
 #include <print>
+#include <thread>
+#include <chrono>
 
 class NexusGame : public NexusEngine::Application {
 public:
@@ -9,6 +12,18 @@ public:
 
     void OnInit() override {
         std::print("Application Initialized!\n");
+
+        NexusEngine::ECS::Registry registry;
+        auto entity = registry.CreateEntity();
+        std::print("Entity created: {} {} \n", entity.m_Id, entity.m_Generation);
+
+        registry.DestroyEntity(entity);
+        std::print("Entity destroyed: {} {} \n", entity.m_Id, entity.m_Generation);
+        auto entity2 = registry.CreateEntity();
+        std::print("Entity created: {} {} \n", entity2.m_Id, entity2.m_Generation);
+
+        auto entity3 = registry.CreateEntity();
+        std::print("Entity created: {} {} \n", entity3.m_Id, entity3.m_Generation);
     }
     void OnUpdate(float dt) override {
         std::print("Delta Time: {} seconds\n", dt);
@@ -19,6 +34,8 @@ public:
             std::print("Total Time: {} seconds\n", totalTime);
             Quit();
         }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
     void OnShutdown() override {}
 };
